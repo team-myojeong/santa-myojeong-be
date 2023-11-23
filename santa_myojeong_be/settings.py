af976a565fb12cfcedb4a484fc84c6b2a2fa5ba3
zeonga1102 <zeonga1102@gmail.com>
2023-11-23 17:18:12 +0900

    django secret key 외부 파일 저장

--- santa_myojeong_be/settings.py
+++ santa_myojeong_be/settings.py
@@ -11,16 +11,23 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import json
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+secret_file = os.path.join(BASE_DIR, 'secrets.json')
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n(c$$9$!6=yfw1#sf51ep7edfkllnua7d#ct&u=8nv49)g#xpk'
+SECRET_KEY = secrets['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
